d437a48cf2b488dcd3331352f73184cf51063561
devsham3w <sandeepkapil@makersncoders.com>
2017-04-07 06:39:48 +0530

    root to

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'apps#index'
 
   # Example of regular route:
     get 'apps/:id/:domain' => 'apps#view'
